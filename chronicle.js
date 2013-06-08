7ec93a58d815e22379301e8de518ba189c0b4633
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-06-08 14:13:13 +0200

    Add a convenience helper to create a MergeConflict object.

--- chronicle.js
+++ chronicle.js
@@ -676,6 +676,13 @@ Chronicle.Diff = Diff;
 Chronicle.Index = Index;
 Chronicle.Versioned = Versioned;
 
+Chronicle.mergeConflict = function(a, b) {
+  var conflict = new errors.MergeConflict("Merge conflict: " + JSON.stringify(source) +" vs " + JSON.stringify(target));
+  conflict.a = a;
+  conflict.b = b;
+  return conflict;
+}
+
 if (typeof exports === 'undefined') {
   root.Substance.Chronicle = Chronicle;
 } else {
