dd4285a1f4a78e9a5e519deaa82f793bd9af2163
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-07-09 16:07:14 +0200

    Move Chronicle sources into src folder.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('./lib/chronicle_index_impl');
-require('./lib/chronicle_impl');
-require('./lib/adapter/array');
-require('./lib/adapter/text');
-module.exports = require('./lib/chronicle');
+require('./src/chronicle_index_impl');
+require('./src/chronicle_impl');
+require('./src/adapter/array');
+require('./src/adapter/text');
+module.exports = require('./src/chronicle');
