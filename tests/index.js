15dfd7bd276632c7f0a2161b6037d3f709a85476
Michael Aufreiter <michael@substance.io>
2014-02-26 18:02:39 +0100

    Get rid of Substance Store dependency.

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,6 @@ require("./002-diff");
 require("./003-merge");
 require("./004-text");
 require("./005-array");
-require("./006-persistent-index");
 
 // skip this test when no indexedDB is available
 if (typeof windows !== "undefined" && window.indexedDB) {
